48f9c52657a92d073ec711bb2129eccb0b4f4a7e
MizukiOtsuki <45745759+MizukiOtsuki@users.noreply.github.com>
2018-12-28 18:54:32 +0900

    10－3まで終了

---
+++ Practice/src/practice10/User.java
@@ -0,0 +1,26 @@
+package practice10;
+
+public class User {
+
+
+/*
+ * クラス名
+ * 		User
+ * フィールド
+ * 		userId		:	int型
+ * 		userNm		：	String型
+ * 		mail		：	String型
+ * 		password	:	String型
+ * メソッド
+ * 		なし
+ */
+
+	int userId;
+
+	String userNm;
+
+	String mail;
+
+	String password;
+
+}
