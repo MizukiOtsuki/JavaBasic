416610b92531e9a84e71b3e8a502bca754b7f1d0
MizukiOtsuki <45745759+MizukiOtsuki@users.noreply.github.com>
2019-01-19 05:18:35 +0900

    13-4まで終了

--- Practice/src/practice13/ptra13/PTra13_04.java
+++ Practice/src/practice13/ptra13/PTra13_04.java
@@ -6,6 +6,9 @@
  */
 package practice13.ptra13;
 
+import practice13.common.Hero;
+import practice13.common.Slime;
+
 public class PTra13_04 {
 
 	/*
@@ -16,6 +19,14 @@ public class PTra13_04 {
 
 		// ★ HeroインスタンスとSlimeインスタンスを作成し、それぞれの名前に"勇者", "スライム"を設定してください
 
+		Hero hero = new Hero();
+
+		hero.setName("勇者");
+
+
+		Slime slime = new Slime();
+
+		slime.setName("スライム");
 
 		/*
 		 * ★ HeroとSlimeを、どちらかが体力０になるまで戦わせます
@@ -23,6 +34,27 @@ public class PTra13_04 {
 		 * 	●Heroの攻撃 -> ダメージ判定 -> Slimeの攻撃 -> ダメージ判定
 		 * 上記を繰り返し行います
 		 */
+
+
+
+		while(true) {
+			int hAtk = hero.attack();
+			boolean slimeDead = slime.damage(hAtk);
+
+			if(slimeDead) {
+				System.out.println(hero.getName()+"は"+slime.getName()+"との戦闘に勝利した");
+				break;
+			}
+
+			int sAtk = slime.attack();
+			boolean heroDead = hero.damage(sAtk);
+
+			if(heroDead) {
+				System.out.println(slime.getName()+"は"+hero.getName()+"との戦闘に勝利した");
+				break;
+			}
+
+		}
 
 
 		// ★ 勝利した方の出力を行ってください。「○○は■■との戦闘に勝利した」
